a36569108676dff8891db7706e13e51de140a85d
Miguelito90 <migueherazo2004@gmail.com>
2026-04-08 11:10:02 -0500

    chore: configuracion inicial del proyecto MiguelFlow

---
+++ src/app.js
@@ -0,0 +1,15 @@
+export function saludar(nombre){
+return "Hola" + nombre + ", bienvenido a aplicacion de MiguelFlow!";
+}
+
+export function despedir(nombre){
+return "hasta pronto," + nombre + "!MiguelFlow";
+}
+
+export function estadoSistema() {
+return {
+estado: "activo",
+version:"1.0.0",
+message: "Sistema MiguelFlow funcionando correctamente"
+ };
+}
---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+import { saludar, despedir, estadoSistema } from "../src/app.js"
+
+function ejecutarPruebas() {
+    let pasadas = 0;
+    let fallidas = 0;
+
+    const r1 = saludar("Miguel")
+    if (r1.includes("Miguel")) {
+        console.log("Test 1 pasado: funcion saludar en CodeMiguel es correcta")
+        pasadas++;
+    } else {
+        console.log("Test 1 Fallido:", r1);
+        fallidas++;
+    }
+
+    const actualEstado = estadoSistema();
+    if (actualEstado.estado === "activo") {
+        console.log("Test 2: Pasado: estadoSistema() funciona en Miguel");
+        pasadas++;
+    } else {
+        console.log("Test 2 Fallido:", estado, "en Miguel");
+        fallidas++;
+    }
+    console.log("\nResultados: " + pasadas + " pasadas,", + fallidas + "fallidas");
+    if (fallidas > 0) process.exit(1);
+}
+ejecutarPruebas();
