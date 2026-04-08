695d1c947b1196addeac113a7fdfba9c9f9e2826
Miguelito90 <migueherazo2004@gmail.com>
2026-04-08 12:22:52 -0500

    se creo la suma

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,4 @@
-import { saludar, despedir, estadoSistema,sumar } from "../src/app.js"
+import { saludar, despedir, estadoSistema, sumar, restar } from "../src/app.js"
 
 function ejecutarPruebas() {
     let pasadas = 0;
@@ -18,21 +18,46 @@ function ejecutarPruebas() {
         console.log("Test 2: Pasado: estadoSistema() funciona en Miguel");
         pasadas++;
     } else {
-        console.log("Test 2 Fallido:", estado, "en Miguel");
+        console.log("Test 2 Fallido:", actualEstado, "en Miguel");
         fallidas++;
     }
-    const sumarResultado = sumar (2,3);
-    if(sumarResultado === 5){
-    console.log("Test 3 pasado: funcion sumar funciona correctamente");
-    pasada++;
+    
+    // Test sumar mostrando resultado
+    const a = 35;
+    const b = 58;
+
+    const sumarResultado = sumar(a, b);
+
+    console.log("Resultado de sumar:", a, "+", b, "=", sumarResultado);
+
+    if (sumarResultado === a + b) {
+        console.log("Test 3 pasado: funcion sumar funciona correctamente");
+        pasadas++;
     } else {
-    console.log ("Test 3 Fallido:", sumaResultado);
-    fallidas++;
+        console.log("Test 3 Fallido:", sumarResultado);
+        fallidas++;
     }
 
-    console.log("\nResultados: " + pasadas + " pasadas,", + fallidas + "fallidas");
+
+    const c = 5;
+    const d = 4;
+    
+    const restarResultado = restar(c, d);
+
+    console.log("Resultado de restar:", c, "-", d, "=", restarResultado);
+    if (restarResultado === c - d){
+        console.log ("Test 4 pasado: funcion restar funcion correctamente");
+        pasadas++;
+    } else {
+        console.log("Test 4 Fallido:", restarResultado);
+        fallidas++;
+    }
+
+    console.log("\nResultados:", pasadas, "pasadas,", fallidas, "fallidas");
+
     if (fallidas > 0) process.exit(1);
+
 }
 
 
-
+ejecutarPruebas();
